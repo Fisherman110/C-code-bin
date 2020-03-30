6ad0947cb33147804a0572571b5447965cde60d0
Fisherman110 <2652134385@qq.com>
2020-03-30 18:22:08 +0800

    测试了一下make的生成

---
+++ 测试make/func1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void func1()
+{
+	printf("this is the func1\n");
+	
+}
---
+++ 测试make/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("this is main fuc\n");
+	func1();
+	func2();
+	func3();
+	return 0;
+}
